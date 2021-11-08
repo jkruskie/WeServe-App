001e8942a3fb3643960b148876ae4f54d63dcec1
Justin Kruskie <justin@jkruskie.com>
2021-11-07 19:49:54 -0500

    Basic Routing /w Icons and default pages.
    Basic routing with the given 5 pages per the design. Started development of home page. NS: Develop pages.

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import BookmarkScreen from './components/BookmarkScreen';
+import HomeScreen from './components/HomeScreen';
+import LogScreen from './components/LogScreen';
+import MessageScreen from './components/MessageScreen';
+import SearchScreen from './components/SearchScreen';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+      
+      initialRouteName="Home"
+
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          size = 25;
+
+          if (route.name === 'Search') {
+            iconName = focused
+              ? 'search'
+              : 'search-outline';
+          } else if (route.name === 'Bookmark') {
+            iconName = 'bookmark';
+          } else if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Logs') {
+            iconName = 'folder';
+          } else if (route.name === 'Messages') {
+            iconName = 'chatbubble';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+
+        },          
+        headerShown: false,
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+      
+      
+    >
+      <Tab.Screen name="Search" component={ SearchScreen } />
+      <Tab.Screen name="Bookmark" component={ BookmarkScreen } />
+      <Tab.Screen name="Home" component={ HomeScreen } />
+      <Tab.Screen name="Logs" component={ LogScreen } />
+      <Tab.Screen name="Messages" component={ MessageScreen } />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/BookmarkScreen.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export default class BookmarkScreen extends Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Bookmark Screen</Text>
+        </View>
+      )
+    }
+  }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize:16,
+    fontWeight:'500',
+    color:'#212121',
+    textAlign:'center'
+  },
+  button: {
+    width:300,
+    borderRadius: 25,
+    backgroundColor:'#FCE4EC',
+    marginVertical: 10,
+    paddingVertical:16
+
+  }
+});
